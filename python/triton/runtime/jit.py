717c3e21c5a9012d1b46481ee537626740d4fd0e
starrryz <760668919@qq.com>
2026-03-06 15:15:32 +0800

    [HINT] Triton v3.5.x hint manager (#349)
    * add hint_manager for cuda
    
    * Apply code-format changes
    
    * update hintmanager : remove backend&version config and so on
    
    * Apply code-format changes
    
    * add TODO, remove priority 3
    
    * Apply code-format changes
    
    ---------
    
    Co-authored-by: flagtree-bot <flagtree_ai@163.com>

--- python/triton/runtime/jit.py
+++ python/triton/runtime/jit.py
@@ -523,18 +523,11 @@ class JITCallable:
     # the user might want to monkey-patch self.src dynamically.
     # Our unit tests do this, for example.
     def parse(self):
+        from ..compiler.hint_manager import hint_trigger
         # Maps line numbers to comment hints
-        line_flagtree_hints = {}
-        code_str = self.src
-        g = tokenize.generate_tokens(StringIO(code_str).readline)
-        for tok_type, tok_text, start, end, _ in g:
-            if tok_type == tokenize.COMMENT:
-                comment = tok_text.replace(" ", "").strip()
-                if comment.startswith('#@hint:'):
-                    flagtree_hints = comment[len('#@hint:'):].strip()
-                    # Record the line number of the comment
-                    line_num = start[0]
-                    line_flagtree_hints[line_num] = flagtree_hints
+        line_flagtree_hints = hint_trigger("maps_line_numbers_to_comment_hints", self)
+        if line_flagtree_hints is None:
+            line_flagtree_hints = {}
 
         tree = ast.parse(self._src)
         assert isinstance(tree, ast.Module)
@@ -542,7 +535,7 @@ class JITCallable:
         assert isinstance(tree.body[0], ast.FunctionDef)
 
         # Attach the line number to comment mapping to the function definition node
-        tree.body[0].line_flagtree_hints = line_flagtree_hints
+        hint_trigger("attach_line_number_to_comment_mapping", tree, line_flagtree_hints)
         return tree
 
     @property
