5189e52488726db3662eddb450e1f3f35fb7bd0f
QiLin Gai <qilin.gai@mthreads.com>
2026-04-22 11:49:53 +0800

    [BACKEND] Initializing the mthreads backend in version triton3.2.x

---
+++ third_party/mthreads/python/test/sqmma/tlLoad_sqmma_tlStore/test_tmeLoad_sqmma_tlStore.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pytest
+import torch
+import os
+import triton
+import triton.language as tl
+from triton.tools.experimental_descriptor import create_2d_tma_descriptor, create_3d_tma_descriptor
+
+
+def get_resolution(dtype):
+    rtol_resolution_map = {
+        torch.float16:
+        1e-3,
+        torch.bfloat16:
+        7.9e-3,
+        # f32->fp8 implemention in triton could ran out of registers when block_size > 256*256*64,
+        # thus convert f32 to f16 first in triton, then convert f16 to dtype after triton kernel.
+        # but this cause some precision problem, thus here use 1.25e-1 as resolution of fp8.
+        # torch.float8_e4m3fn: 1e-3,
+        torch.float8_e4m3fn:
+        1.25e-1,
+    }
+    return rtol_resolution_map.get(dtype, None)
+
+
+def get_triton_dtype(dtype):
+    dtype_map = {
+        torch.float16: tl.float16,
+        torch.float32: tl.float32,
+        torch.bfloat16: tl.bfloat16,
+        torch.float8_e4m3fn: tl.float8e4nv,
+    }
+    return dtype_map.get(dtype, None)
+
+
+@triton.jit
+def matmul_kernel_tmeLoad_sqmma_tlStore(
+    a_desc_ptr,
+    b_desc_ptr,
+    C_ptr,  #
+    M,
+    N,
+    K,
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_K: tl.constexpr,
+    dtype: tl.constexpr,
+    save_dtype: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    pid_m = pid % num_pid_m
+    pid_n = pid // num_pid_m
+    offs_am = pid_m * BLOCK_SIZE_M
+    offs_bn = pid_n * BLOCK_SIZE_N
+    offs_k = 0
+
+    # handle an AttributeError
+    dtype = dtype
+
+    # pointers
+    offs_m = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_n = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_block_ptr = C_ptr + N * offs_m[:, None] + 1 * offs_n[None, :]
+
+    mask = ((offs_am + tl.arange(0, BLOCK_SIZE_M))[:, None] < M) & ((offs_bn + tl.arange(0, BLOCK_SIZE_N)[None, :]) < N)
+
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        a = tl._experimental_descriptor_load(a_desc_ptr, [offs_am, offs_k], [BLOCK_SIZE_M, BLOCK_SIZE_K], dtype)
+        b = tl._experimental_descriptor_load(b_desc_ptr, [offs_k, offs_bn], [BLOCK_SIZE_K, BLOCK_SIZE_N], dtype)
+        accumulator = tl.dot(a, b, acc=accumulator)
+        offs_k += BLOCK_SIZE_K
+    # f32->bf16 implemention in triton could ran out of registers when block_size > 128*128*64,
+    # thus convert f32 to f16 first in triton, then convert f16 to dtype after triton kernel.
+    accumulator = accumulator.to(save_dtype)
+    tl.store(c_block_ptr, accumulator, mask=mask)
+
+
+@pytest.mark.parametrize("num_stages", [1])
+@pytest.mark.parametrize(
+    "M, N, K",
+    [
+        (4096, 3072, 2048),
+        (4096, 576, 2048),
+        (4096, 4096, 512),
+        (4096, 2048, 2048),
+        (4096, 5632, 2048),
+        (4096, 2048, 2816),
+        (4096, 2816, 2048),
+        (2048, 2816, 4096),
+        (4096, 2048, 5632),
+        (5632, 2048, 4096),
+        (2048, 4096, 2048),
+        (4096, 512, 4096),
+        (4096, 2048, 576),
+        (4096, 2048, 3072),
+        (3072, 2048, 4096),
+        (2048, 2048, 4096),
+    ],
+)
+@pytest.mark.parametrize(
+    "BLOCK_M, BLOCK_N, BLOCK_K, num_warps",
+    [
+        (32, 32, 32, 4),
+        # (128, 128, 64, 4), # failed maybe due to register spill?
+        (64, 64, 32, 16),
+        # (256, 256, 64, 16),
+        (32, 64, 32, 8),
+        (64, 32, 32, 8),
+        # (128, 256, 64, 8),
+        # (256, 128, 64, 8),
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16, torch.float8_e4m3fn])
+def test_tmeLoad_sqmma_tlStore(num_stages, M, N, K, BLOCK_M, BLOCK_N, BLOCK_K, num_warps, dtype):
+    device = "musa"
+    rtol = get_resolution(dtype)
+    save_dtype = torch.float16
+    torch.manual_seed(42)
+    A = torch.randn((M, K), dtype=torch.float32, device=device).to(dtype)
+    B = torch.randn((K, N), dtype=torch.float32, device=device).to(dtype)
+    C = torch.zeros((M, N), dtype=torch.float32, device=device).to(save_dtype)
+
+    desc_a = create_2d_tma_descriptor(A.data_ptr(), M, K, BLOCK_M, BLOCK_K, A.element_size())
+    desc_b = create_2d_tma_descriptor(B.data_ptr(), K, N, BLOCK_K, BLOCK_N, B.element_size())
+
+    kernel = matmul_kernel_tmeLoad_sqmma_tlStore[(triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N), 1, 1)](
+        desc_a,
+        desc_b,
+        C,
+        M,
+        N,
+        K,
+        BLOCK_M,
+        BLOCK_N,
+        BLOCK_K,
+        get_triton_dtype(dtype),
+        get_triton_dtype(save_dtype),
+        num_warps=num_warps,
+        num_stages=num_stages,
+    )
+    torch.set_printoptions(threshold=float("inf"), linewidth=800, edgeitems=50, sci_mode=False, precision=1)
+    C = C.to(dtype)
+    ref_out = torch.matmul(A, B).to(dtype)
+    torch.testing.assert_close(ref_out.to(torch.float32), C.to(torch.float32), rtol=rtol, atol=1e-3)
